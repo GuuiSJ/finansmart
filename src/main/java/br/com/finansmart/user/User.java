14c225940d25bc3fe2478303527571666afbcbd1
GuuiSJ <djesus.guilherme082@gmail.com>
2025-08-25 20:27:31 -0300

    Primeiro commit

---
+++ src/main/java/br/com/finansmart/user/User.java
@@ -0,0 +1,24 @@
+package br.com.finansmart.user;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "User")
+@Table(name = "users")
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String password;
+}

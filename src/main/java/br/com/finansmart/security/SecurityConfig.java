1ed5a3179ecf927c23df770f3cc19f3e384a5a31
GuuiSJ <djesus.guilherme082@gmail.com>
2025-08-27 19:29:36 -0300

    feat: Adiciona criptografia de senha no cadastro de usuário

--- src/main/java/br/com/finansmart/security/SecurityConfig.java
+++ src/main/java/br/com/finansmart/security/SecurityConfig.java
@@ -6,21 +6,27 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration // Indica que esta é uma classe de configuração do Spring
-@EnableWebSecurity // Ativa a segurança web do Spring
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean // Indica que este método produz um "Bean" a ser gerido pelo Spring
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable()) // Desativa CSRF, pois não usamos sessões
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Configura a gestão de sessão como sem estado (stateless)
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/api/users").permitAll() // PERMITE todos os pedidos POST para /api/users
-                        .anyRequest().authenticated() // EXIGE autenticação para qualquer outro pedido
+                        .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/br/com/finansmart/user/UserService.java
+++ src/main/java/br/com/finansmart/user/UserService.java
@@ -1,17 +1,24 @@
 package br.com.finansmart.user;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository  userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public User createUser(User user) {
+        String encodedPassword = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
         return userRepository.save(user);
     }
 }
